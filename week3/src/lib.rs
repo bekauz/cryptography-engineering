379514b0c1e6ab2e30f5e78cb4c65eef9febcaef
bekauz <bekauz@protonmail.com>
2022-12-09 19:42:48 +0100

    5.3

--- week3/src/lib.rs
+++ week3/src/lib.rs
@@ -1,26 +1,33 @@
 mod ch3 {
-    use sha2::{Sha256, Sha512, Digest};
-    use hex_literal::hex;
-    use std::collections::HashSet;
+    use sha2::{Sha512, Digest};
+    use std::collections::{HashMap};
 
     pub fn sha_512_n(m: &[u8], n: usize) -> Vec<u8> {
         let mut hasher = Sha512::new();
         hasher.update(m);
         let hash = hasher.finalize();
-        let result = hash[0..n].to_vec();
-        result
+        let take_bytes: usize = n / 8;
+        let result = hash.iter().take(take_bytes).copied();
+        result.collect()
     }
 
-    pub fn birthday_attack_sha_512_n(input: Vec<u8>, n: usize) {
-
-        let mut found = false;
-        while !found {
-            // let data = ...
-            // let sha_512_n_hash = sha_512_n(data.as_slice(), n);
-            // if input == sha_512_n_hash {
-            //     found = true;
-            //     println!("COLLISION WITH {:?}", data);
-            // }
+    pub fn birthday_attack_sha_512_n(n: usize) {
+        // map of hash value -> int
+        let mut previous = HashMap::new();
+        for i in 1usize .. 100_000 {
+            let hash = sha_512_n(&i.to_be_bytes(), n);
+            let resp = previous.insert(hash.clone(), i);
+            match resp {
+                None => {}
+                Some(val) => {
+                    println!("collision: {:?} and {:?} both map to {:?}",
+                             i,
+                             val,
+                             &hash
+                    );
+                    break;
+                }
+            }
         }
     }
 
@@ -38,11 +45,8 @@ mod ch3 {
 
         #[test]
         fn bday_attack_sha_512_n() {
-            let bytes = b"hello hello";
-            let n = 8;
-            let sha_n_bytes = sha_512_n(bytes, n);
-
-            birthday_attack_sha_512_n(sha_n_bytes, n);
+            let n = 24;
+            birthday_attack_sha_512_n(n);
         }
     }
 }
