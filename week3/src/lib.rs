d68ee2353d40e873eddd914ff6add094c6ea44d1
bekauz <bekauz@protonmail.com>
2022-12-10 21:44:03 +0100

    week 3

--- week3/src/lib.rs
+++ week3/src/lib.rs
@@ -1,4 +1,4 @@
-mod ch3 {
+mod ch5 {
     use sha2::{Sha512, Digest};
     use std::collections::{HashMap};
 
@@ -31,6 +31,27 @@ mod ch3 {
         }
     }
 
+    pub fn find_sha_512_16_message_attempts(hash_hex: &[u8; 2], offset: usize) -> usize {
+        let mut i = offset;
+        loop {
+            let hash = sha_512_n(&i.to_be_bytes(), 16);
+            if hash == hash_hex.to_vec() {
+                println!("collision with msg: {:?} @ i:{:?}", &i.to_be_bytes(), i);
+                break;
+            }
+            i += 1;
+        }
+        i - offset
+    }
+
+    pub fn average_sha_512_16_attempts(hash_hex: &[u8; 2]) -> usize {
+        let offsets: Vec<usize> = vec![1597, 28657, 514229, 433494437, 2971215073];
+        let total_ops: usize = offsets.iter()
+            .map(|offset| find_sha_512_16_message_attempts(hash_hex, offset.clone()))
+            .sum();
+        total_ops / offsets.len()
+    }
+
     #[cfg(test)]
     mod tests {
         use super::*;
@@ -47,6 +68,14 @@ mod ch3 {
         fn bday_attack_sha_512_n() {
             let n = 24;
             birthday_attack_sha_512_n(n);
+        }
+
+        #[test]
+        fn find_message_by_hash() {
+            // hex value to find
+            let s: [u8; 2] = [0x3D, 0x4B];
+            let avg = average_sha_512_16_attempts(&s);
+            println!("average attempts: {:?}", avg);
         }
     }
 }
